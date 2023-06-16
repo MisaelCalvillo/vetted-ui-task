65c6102c6fee6c5575950ea4cab98461acd625b6
Misael Calvillo Mancilla <misa@careme.mx>
2023-06-16 10:05:59 -0600

    Add text for input

--- src/components/ChatTextInput.test.tsx
+++ src/components/ChatTextInput.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import ChatTextInput from './ChatTextInput';
 
@@ -11,14 +11,21 @@ describe('ChatTextInput', () => {
 
   };
 
+  const writer = {
+    name: "Misael",
+    photo: "https://s3-alpha-sig.figma.com/img/dc43/b407/978aff670ff4902adb81a3ec058e1730?Expires=1687737600&Signature=W2ODZx0vKMB1cYZdulLA3ok2s4twbHyMDN5~xrlr4so9ZGdWvXvXKoJ1XAMvVByF5IVNYABUUKLiHwsCGogXVuF~kmEhi48qHUxTXJ0rBaQGS93z22TqR7j7d5oYa45HywDcSFx6eFgpD52S~JCSrGWEsCn~WLPCcmE252xshYrU8PxFNY3UPtU6L1tRgbiu2xL7JVAEUIfiDcdrFSYOjhm9mJq2fmdtvxfO3hR7KW0gt~dFHbLs2WXMYOIIe~P8V2-xvuR1PWuiLFaVM8sB2C2AtoEUVwvLBZTVyXiZ7E-ecv9DOybKji2YZN9yUzalDEcki0R9YXkukug-lKmNXw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+  }
+
   it('renders on the screen', () => {
     render(<ChatTextInput {...defaultProps} />)
+    expect(screen.getByText("Assign myself and reply")).toBeInTheDocument();
   });
 
-  // it('renders the initial value in the input field', () => {
-  //   const { getByRole } = render(<ChatTextInput {...defaultProps} value="Initial value" />);
-  //   expect(getByRole('textbox')).toHaveValue('Initial value');
-  // });
+  it('renders the initial value in the input field', () => {
+    render(<ChatTextInput {...defaultProps} writer={writer} value="Initial value" />);
+    screen.debug()
+    expect(screen.getByRole('textbox')).toHaveValue('Initial value');
+  });
 
   // it('updates the input field as user types', () => {
   //   const { getByRole } = render(<ChatTextInput {...defaultProps} />);
